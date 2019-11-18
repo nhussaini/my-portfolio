c36853c82bacafc11aff7e2de27eba13d1f43148
unknown <nhussaini@luc.edu>
2019-11-18 12:17:22 -0800

    added transition to the list

--- src/app/components/portfolio/portfolio.component.ts
+++ src/app/components/portfolio/portfolio.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 import { PortfolioService } from 'src/app/services/portfolio.service';
 import { Portfolio } from 'src/app/services/portfolio.model';
-import { trigger, transition, style, animate } from '@angular/animations';
+import { trigger, transition, style, animate, animateChild, stagger, query } from '@angular/animations';
 
 @Component({
   selector: 'app-portfolio',
   templateUrl: './portfolio.component.html',
   styleUrls: ['./portfolio.component.sass'],
   animations: [
+    trigger('list', [
+      transition(':enter', [
+        query('@items', stagger(60, animateChild()))
+      ])
+    ]),
     trigger('items', [
-      transition('void => *', [
+      transition(':enter', [
         style({ transform: 'scale(0)', opacity: 0}),
         animate('0.350s cubic-bezier(.8, -0.6, 0.2, 1.5)',
         style({ transform: 'scale(1)', opacity: 1 }))
