7033674ccce9ec3958f34a4ebf853a31a9096541
unknown <nhussaini@luc.edu>
2019-11-17 21:02:32 -0800

    added filter option to portfolios

--- src/app/components/portfolio/portfolio.component.ts
+++ src/app/components/portfolio/portfolio.component.ts
@@ -9,13 +9,27 @@ import { Portfolio } from 'src/app/services/portfolio.model';
 })
 export class PortfolioComponent implements OnInit {
   portfolios: Portfolio[];
-  selectedType: 'all' | 'angular' | 'react' | 'vue' = 'all'; // by default, it is all
+  private _selectedType: 'all' | 'Angular' | 'React' | 'Vue' = 'all'; // by default, it is all
+
+  get selectedType() {
+    return this._selectedType;
+  }
+  set selectedType(newValue: 'all' | 'Angular' | 'React' | 'Vue') {
+    if (newValue !== this._selectedType) {
+      this._selectedType = newValue;
+      this.loadPortfolios(this._selectedType);
+    }
+  }
 
   constructor(private portfolioSvc: PortfolioService) { }
 
   ngOnInit() {
+    this.loadPortfolios(this._selectedType);
+  }
+
+  loadPortfolios(selectedType: string): void {
     this.portfolioSvc.get().subscribe(data => {
-      this.portfolios = data;
+      this.portfolios = data.filter(p => p.type === selectedType || selectedType === 'all');
     });
   }
 
